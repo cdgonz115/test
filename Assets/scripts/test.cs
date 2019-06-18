126704decaf457e542d557f19d87ca74efbb32ea
propredator23 <christiandash@hotmail.com>
2019-06-17 22:09:25 -0700

    test

---
+++ Assets/scripts/test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public int lol=0;
+	// Update is called once per frame
+	void Update ()
+    {
+        increase(5);
+	}
+    void increase(int passed)
+    {
+        lol = lol + passed;
+    }
+}
